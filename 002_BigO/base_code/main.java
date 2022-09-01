9e9770c5ba8b9fc6ccec3c9fcf272ab2c54a99b3
will0422 <williamchin2005@gmail.com>
2022-09-01 11:48:20 -0700

    finished lab 2

--- 002_BigO/base_code/main.java
+++ 002_BigO/base_code/main.java
@@ -20,11 +20,15 @@ class main {
 			System.out.println("Interval: " + i);
 
 			nums = new int[i];
+			randomize(nums);
 			start = System.nanoTime();
 
 			// Put your method between these two comments
-			// randomize(nums);
-			search(nums);
+
+			// search(nums);
+			// bubble(nums);
+			// insertion(nums);
+			selection(nums);
 			//
 
 			finish = System.nanoTime();
@@ -56,5 +60,48 @@ class main {
 			}
 		}
 		return found;
+	}
+
+	public static void bubble(int[] arr) {
+		int n = arr.length;
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (arr[j - 1] > arr[j]) {
+					// swap elements
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+
+			}
+		}
+	}
+
+	public static void insertion(int[] array) {
+		int n = array.length;
+		for (int j = 1; j < n; j++) {
+			int key = array[j];
+			int i = j - 1;
+			while ((i > -1) && (array[i] > key)) {
+				array[i + 1] = array[i];
+				i--;
+			}
+			array[i + 1] = key;
+		}
+	}
+
+	public static void selection(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int index = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[index]) {
+					index = j;// searching for lowest index
+				}
+			}
+			int smallerNumber = arr[index];
+			arr[index] = arr[i];
+			arr[i] = smallerNumber;
+		}
 	}
 }
